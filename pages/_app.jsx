4c99dc9559965b7d8d31392b7624487790604b7d
safwanrazali <a.safwanrazali@gmail.com>
2026-07-22 12:05:27 +0800

    chore: update dependencies and refactor project structure
    - Updated Next.js to version 16.2.12 and added new dependencies including axios, bootstrap, date-fns, mongoose, next-auth, react-calendar, react-hook-form, sass, xlsx, and yup.
    - Removed the old _app.js file and replaced it with _app.jsx to include Bootstrap and global styles.
    - Created a new _document.jsx file for custom document structure.
    - Deleted the unused API route at pages/api/hello.js.
    - Added a new index.jsx file with a basic homepage layout and styles.
    - Introduced SCSS for global styles and created variables and mixins files for better styling management.

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "@/styles/globals.scss";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
